847abc3556a7bab4a78af27013ad4981eccbc638
sourovmmoysanju <sourovmmoysanju@gmail.com>
2025-12-19 15:22:15 +0600

    add backend server

--- src/Components/Hooks/useAxios.jsx
+++ src/Components/Hooks/useAxios.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import React from "react";
 
-const instance = axios.create({
-  baseURL: "http://localhost:3000",
-});
 const useAxios = () => {
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_SERVER_LINK,
+  });
+
   return instance;
 };
 
